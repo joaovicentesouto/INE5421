73fb874ce48378cd6f81c07c36d5599433cd7be2
João Vicente Souto <joaovicentesouto@gmail.com>
2018-05-15 13:03:18 -0300

    Regular Expression Parser
    Some tests realized

--- formal_languages/parsers/ExpressionParser.cpp
+++ formal_languages/parsers/ExpressionParser.cpp
@@ -5,5 +5,94 @@ namespace formal_device
 namespace parser
 {
 
+regular_ptr make_regular_expression(const string_type & file_path)
+{
+    std::ifstream t(file_path);
+    string_type exp((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+
+    //exp.erase( std::remove_if(exp.begin(), exp.end(), std::isspace), exp.end());
+
+    string_iterator_type begin = exp.begin();
+    string_iterator_type end   = exp.end();
+
+    regular_ptr current_exp(new empty_type());
+
+    while (begin != end)
+        if (*begin == '|')
+            current_exp = current_exp | parse(begin, end);
+        else
+            current_exp = current_exp + parse(begin, end);
+
+    return current_exp;
+}
+
+regular_ptr parse(string_iterator_type &begin, const string_iterator_type &end)
+{
+    if (begin == end)
+        return new empty_type();
+
+    string_type caracter(&(*begin), &(*begin) + 1);
+
+    if (*begin == '(')
+    {
+        auto exp = parse(++begin, end);
+
+        if (*begin != ')')
+            throw std::out_of_range("Expressão mal formada");
+
+        switch (*begin) {
+        case '*':
+            return exp ^ expression::Operation::Star;
+
+        case '+':
+            return exp ^ expression::Operation::Plus;
+
+        case '?':
+            return exp ^ expression::Operation::Optional;
+
+        default:
+            return exp;
+        }
+    }
+    else if (std::regex_match(caracter, std::regex("[a-z0-9]")))
+    {
+        regular_ptr exp(new unit_type(*begin));
+
+        ++begin;
+
+        if (begin == end)
+            return exp;
+
+        caracter = string_type(&(*begin), &(*begin) + 1);
+
+        if (std::regex_match(caracter, std::regex("[a-z0-9]")) || *begin == '(')
+            return exp + parse(begin, end);
+
+        switch (*begin) {
+        case '*':
+            exp = exp ^ expression::Operation::Star;
+
+        case '+':
+            exp = exp ^ expression::Operation::Plus;
+
+        case '?':
+            exp = exp ^ expression::Operation::Optional;
+
+        default:
+            break;
+        }
+
+        if (*(++begin) == '|')
+            return exp | parse(begin, end);
+        else
+            return exp + parse(begin, end);
+    }
+
+    if (*begin == '*' || *begin == '+' || *begin == '+' || *begin == ')')
+        throw std::out_of_range("Expressão mal formada");
+
+    return new empty_type();
+}
+
 } // namespace parser
 } // namespace formal_device
--- formal_languages/parsers/ExpressionParser.hpp
+++ formal_languages/parsers/ExpressionParser.hpp
@@ -1,73 +1,37 @@
 #ifndef PARSER_REGULAREXPRESSION_HPP
 #define PARSER_REGULAREXPRESSION_HPP
 
+#include <string>
 #include <iostream>
-//#include <boost/spirit/home/x3.hpp>
-//#include <boost/fusion/include/adapt_struct.hpp>
-//#include <boost/fusion/include/io.hpp>
+#include <fstream>
+#include <streambuf>
+#include <algorithm>
+#include <regex>
 
-//#include <boost/spirit/include/support_istream_iterator.hpp>
-//#include <iostream>
-//#include <sstream>
+#include <formal_languages/devices/RegularExpressionComponents.hpp>
 
-//namespace formal_devices
-//{
-//namespace expression
-//{
-//namespace ast
-//{
+namespace formal_device
+{
+namespace parser
+{
 
-//struct rexpr
-//{
-//    std::string m_left{""};
-//    std::string m_operation{"."};
-//    std::string m_right{""};
-//};
+using string_type          = expression::string_type;
+using string_iterator_type = string_type::iterator;
+using regular_ptr          = expression::RegularPointer;
+using empty_type           = expression::Empty;
+using epsilon_type         = expression::Epsilon;
+using unit_type            = expression::Unit;
+using union_type           = expression::Union;
+using concatenation_type   = expression::Concatenation;
+using reflexive_type       = expression::ReflexiveClosure;
+using transitive_type      = expression::TransitiveClosure;
+using optional_type        = expression::Optional;
 
-//}
-//}
-//}
+regular_ptr make_regular_expression(const string_type & file_path);
 
-//BOOST_FUSION_ADAPT_STRUCT(formal_devices::expression::ast::rexpr, m_left, m_operation, m_right)
+regular_ptr parse(string_iterator_type &begin, const string_iterator_type &end);
 
-//namespace formal_devices
-//{
-//namespace expression
-//{
-//namespace parser {
-//namespace x3 = boost::spirit::x3;
-//    namespace ascii = boost::spirit::x3::ascii;
-
-//    using x3::lit;
-//    using x3::lexeme;
-//    using ascii::char_;
-//    using ascii::string;
-
-//    x3::rule<class rexpr, ast::rexpr>       rexpr = "rexpr";
-//    x3::rule<class rexpr_value, ast::rexpr> rexpr = "rexpr";
-
-//    auto const closure =
-//        '*' | '+' | '?';
-
-//    auto const operation = '|';
-
-//    auto const symbol =
-//        char_("a-z");
-
-//    auto const rexpr_value_def =
-//        symbol >> symbol | symbol | rexpr;
-
-//    auto const rexpr_key_value_def =
-//        symbol >> '|' >> rexpr_value;
-
-//    auto const rexpr_def =
-//        '(' >> rexpr >> ')' >> closure |
-//        rexpr >> closure |
-//        rexpr_value;
-
-//    BOOST_SPIRIT_DEFINE(rexpr_value, rexpr, rexpr_key_value);
-//}
-//}
-//}
+}   // namespace parser
+}   // namespace formal_device
 
 #endif
